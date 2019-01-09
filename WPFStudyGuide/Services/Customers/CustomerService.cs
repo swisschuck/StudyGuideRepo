93feac6a95ef2cdaddd384b0f447001468a6132e
Charlie <chuegi@kiosk.com>
2019-01-09 10:14:13 -0700

    MVVM setup

---
+++ WPFStudyGuide/Services/Customers/CustomerService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFStudyGuide.Classes;
+
+namespace WPFStudyGuide.Services.Customers
+{
+    public class CustomerService : ICustomerService
+    {
+        #region fields
+        #endregion fields
+
+
+        #region properties
+        #endregion properties
+
+
+        #region constructors
+        #endregion constructors
+
+
+        #region public methods
+
+        public Task<List<Customer>> GetCustomersAsync()
+        {
+            Task<List<Customer>> taskToReturn = Task.Run(() =>
+            {
+                return new List<Customer>();
+            });
+
+            return taskToReturn;
+        }
+
+        public Task<Customer> GetCustomerAsync(Guid id)
+        {
+            Task<Customer> taskToReturn = Task.Run(() =>
+            {
+                return new Customer();
+            });
+
+            return taskToReturn;
+        }
+
+        public async Task<Customer> AddCustomerAsync(Customer customer)
+        {
+            return new Customer();
+        }
+
+        public async Task<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            return new Customer();
+        }
+
+        public async Task<bool> DeleteCustomerAsync(Guid customerId)
+        {
+            return true;
+        }
+        #endregion public methods
+
+
+        #region private methods
+        #endregion private methods
+    }
+}
---
+++ WPFStudyGuide/Services/Customers/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFStudyGuide.Classes;
+
+namespace WPFStudyGuide.Services.Customers
+{
+    interface ICustomerService
+    {
+        Task<List<Customer>> GetCustomersAsync();
+        Task<Customer> GetCustomerAsync(Guid id);
+        Task<Customer> AddCustomerAsync(Customer customer);
+        Task<Customer> UpdateCustomerAsync(Customer customer);
+        Task<bool> DeleteCustomerAsync(Guid customerId);
+    }
+}
