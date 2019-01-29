2652ef8f982480a6d15dba67ca02cf89ba767557
chuegi <swiss.chuck1983@gmail.com>
2019-01-28 18:41:16 -0700

    add/edit customer complete with validation

---
+++ WPFStudyGuide/Converters/NegatableBooleanToVisibilityConverter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFStudyGuide.Converters
+{
+    public class NegatableBooleanToVisibilityConverter : IValueConverter
+    {
+        // Negatable - to deny the existence or truth of
+        //           - to nullify or make ineffective
+
+        #region fields
+        #endregion fields
+
+
+        #region properties
+
+        public bool Negate { get; set; }
+        public Visibility FalseVisibility { get; set; }
+
+        #endregion properties
+
+
+        #region constructors
+
+        public NegatableBooleanToVisibilityConverter()
+        {
+            FalseVisibility = Visibility.Collapsed;
+        }
+
+        #endregion constructors
+
+
+        #region public methods
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool booleanValue;
+            bool booleanConvertResult = bool.TryParse(value.ToString(), out booleanValue);
+
+
+            if (!booleanConvertResult)
+            {
+                return value;
+            }
+
+            if (booleanValue && !Negate)
+            {
+                return Visibility.Visible;
+            }
+
+            if (booleanValue && Negate)
+            {
+                return FalseVisibility;
+            }
+
+            if (!booleanValue && Negate)
+            {
+                return Visibility.Visible;
+            }
+
+            if (!booleanValue && !Negate)
+            {
+                return FalseVisibility;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion public methods
+
+
+        #region private methods
+        #endregion private methods
+    }
+}
