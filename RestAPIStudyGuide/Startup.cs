d412ff451d5b98725cbe364c4b241e9767b3a12b
chuegi <swiss.chuck1983@gmail.com>
2019-02-06 16:48:43 -0800

    rest API module 1 complete

--- RestAPIStudyGuide/Startup.cs
+++ RestAPIStudyGuide/Startup.cs
@@ -11,17 +11,21 @@ namespace RestAPIStudyGuide
 {
     public class Startup
     {
+        // notes that came with this file:
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
+        // notes that came with this file:
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                // ch - adding DeveloperException "Middleware" in the pipeline
+                // this is only added if the environment is in development
                 app.UseDeveloperExceptionPage();
             }
 
